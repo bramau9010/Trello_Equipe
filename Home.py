e81b47c5b775ee759105e9a95b0bb65ffd166776
bramau9010 <78278384+bramau9010@users.noreply.github.com>
2025-08-03 18:53:32 -0300

    Create Home.py
    Homepage de la app de gestão

---
+++ Home.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+from pathlib import Path
+
+st.sidebar.markdown('Dessenvolvido por [Brayan Maurico Rodríguez](https://sites.google.com/view/brayanmauricio)')
+
+st.markdown(
+'''
+Projeto de  Acompanhamento Gestão de Equipe
+
+'''
+
+)
+
+st.divider()
+
+st.markdown(
+
+'''
+Dash interativo, armazenado em  [GitHub](https://github.com/bramau9010) 
+'''
+)
